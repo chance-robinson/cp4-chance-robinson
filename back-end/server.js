1f29f036218472cbdc7ece7587dad6d94802d1f2
Chance Robinson <brighamyoung2@gmail.com>
2022-03-28 22:45:58 -0600

    completed lab4 museum

--- back-end/server.js
+++ back-end/server.js
@@ -14,5 +14,82 @@ mongoose.connect('mongodb://localhost:27017/museum', {
   useNewUrlParser: true
 });
 
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+const itemSchema = new mongoose.Schema({
+  title: String,
+  desc: String,
+  path: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    desc: req.body.desc,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(504);
+  }
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(503);
+  }
+});
+
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(502);
+  }
+}); 
+
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    let item = await Item.findOne({
+      _id: req.params.id,
+    });
+    item.title = req.body.title;
+    item.desc = req.body.desc;
+    await item.save();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(501);
+  }
+}); 
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
